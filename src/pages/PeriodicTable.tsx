42c32247cdf6614d29c349e8cc3a3f8a13c1e6f4
jvl <jovenvictorlogo@gmail.com>
2023-09-14 22:02:14 +0800

    added darkMode class

--- src/pages/PeriodicTable.tsx
+++ src/pages/PeriodicTable.tsx
@@ -1,6 +1,9 @@
 // packages
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+
+// custom hooks
+import { useTheme } from '../hooks/useTheme';
 
 // data / apis
 import data from '../data/elements.json';
@@ -8,6 +11,10 @@ import categories from '../data/categories';
 
 // other pages
 import HoveredElement from './HoveredElement';
+
+// assets
+import light from '../assets/images/light-mode-toggle-icon.svg';
+import dark from '../assets/images/dark-mode-toggle-icon.svg';
 
 const PeriodicTable = () => {
   // states and hooks
@@ -18,6 +25,16 @@ const PeriodicTable = () => {
   const [highlightedCategory, setHighlightedCategory] = useState<string | null>(
     null
   );
+
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    if (theme === 'dark') {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [theme]);
 
   // functions
   const handleMouseOver = (element: ElementInfoType) => {
@@ -33,9 +50,25 @@ const PeriodicTable = () => {
     setHighlightedCategory(null);
   };
 
+  const toggleSwitch = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark');
+  };
+
   return (
-    <div className='p-4'>
-      <h1 className='text-center text-2xl font-bold'>
+    <div className='p-4 md:px-12 md:py-6 bg-slate-100 dark:bg-slate-600 h-screen'>
+      <button
+        type='button'
+        onClick={toggleSwitch}
+        className='fixed z-10 right-2 top-2 w-16 p-1 rounded-md'
+      >
+        {theme === 'dark' ? (
+          <img src={dark} alt='Dark Mode' />
+        ) : (
+          <img src={light} alt='Light Mode' />
+        )}
+      </button>
+
+      <h1 className='text-center text-2xl font-bold dark:text-white'>
         Periodic Table of Elements
       </h1>
 
