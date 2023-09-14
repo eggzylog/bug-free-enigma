cc9b6903df8dd55c722b653fea90cc2359df82d4
jvl <jovenvictorlogo@gmail.com>
2023-09-14 15:09:05 +0800

    plain periodic table

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import data from './data/elements.json';
+
 const App = () => {
   return (
-    <div className='flex place-items-center place-content-center h-screen'>
-      <h1 className='text-4xl'>Hello World!</h1>
+    <div className='m-4'>
+      <h1 className='text-center text-2xl font-bold'>
+        Periodic Table of Elements
+      </h1>
+      <div className='periodic-table'>
+        {data.elements.map((element) => (
+          <div
+            key={element.name}
+            className='element justify-center'
+            style={{
+              gridRow: element.ypos,
+              gridColumn: element.xpos,
+            }}
+          >
+            <strong>{element.symbol}</strong>
+            <small className='number'>{element.number}</small>
+            <small className='name'>{element.name}</small>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
